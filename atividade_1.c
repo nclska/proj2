d60cc64ea9f8e29a7062aed970db60728e95bd54
nclska <nicolaskalebe223@gmail.com>
2023-03-21 22:42:08 -0300

    segunda contribuição

--- atividade_1.c
+++ atividade_1.c
@@ -3,6 +3,7 @@
 
 int main()
 {
-    printf("RAFAEL DOS SANTOS ALMEIDA (202206840056)\n");
+    printf("RAFAEL DOS SANTOS ALMEIDA - 202206840056\n");
+    printf("Nicolas Kalebe Menezes da Silva - 202207040033\n");
     return 0;
 }
