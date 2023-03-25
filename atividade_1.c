eb15a6c0315a141e1ff3908d251688e727757f94
Wesl96 <128491049+Wesl96@users.noreply.github.com>
2023-03-24 23:33:16 -0300

    Atualizar o atividade_1.c

--- atividade_1.c
+++ atividade_1.c
@@ -4,8 +4,11 @@
 int main()
 {
     printf("RAFAEL DOS SANTOS ALMEIDA - 202206840056\n");
+
     printf("Nicolas Kalebe Menezes da Silva - 202207040033\n");
+
     printf("LUANA MORAIS LISBOA - 202007040022\n");
+
     printf("WESLEY FIGUEIREDO DA SILVA - 202207040025\n");
     return 0;
 }
